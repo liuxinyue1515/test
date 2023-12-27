c913f19d6a26b775421d6021dd4da928efb2e1d8
liuxinyue <liuxinyue1515@gmail.com>
2023-12-27 12:43:31 +0800

    新的master2

--- untitled/src/main/java/GitTest.java
+++ untitled/src/main/java/GitTest.java
@@ -1,5 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("新的master2");
         System.out.println("新的master1");
         System.out.println("新的test2");
     }
