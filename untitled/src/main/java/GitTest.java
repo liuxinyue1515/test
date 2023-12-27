43a99797ea45bbeb5632b4deea3998f713083d09
liuxinyue <liuxinyue1515@gmail.com>
2023-12-27 11:42:21 +0800

    hotfix提交

--- untitled/src/main/java/GitTest.java
+++ untitled/src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("添加GitTest");
         System.out.println("第三次提交");
+        System.out.println("Hot fix修改");
     }
 }
