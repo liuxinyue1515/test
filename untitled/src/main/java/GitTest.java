39fd8a19d655df101a155660d45742c3aa48a8d8
liuxinyue <liuxinyue1515@gmail.com>
2023-12-27 12:41:50 +0800

    测试test1

--- untitled/src/main/java/GitTest.java
+++ untitled/src/main/java/GitTest.java
@@ -1,8 +1,5 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("master+hotfix分支第5次修改");
-        System.out.println("master分支第3次修改");
-        System.out.println("master分支第4次修改");
-        System.out.println("master分支第5次修改");
+        System.out.println("测试test1");
     }
 }
