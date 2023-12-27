bc9c9f923187722efa0dc56c39f09b471335462a
下页随风 <80025236+liuxinyue1515@users.noreply.github.com>
2023-12-27 14:07:36 +0800

    Update GitTest.java

--- untitled/src/main/java/GitTest.java
+++ untitled/src/main/java/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("新的test2");
         System.out.println("push again github");
         System.out.println("pull github");
+
+        System.out.println("hello");
     }
 }
